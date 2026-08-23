f4918e42d005a84de96e4674ee265a0d2d3018a5
Enrico Bianchi <enrico.bianchi@gmail.com>
2026-08-23 23:42:51 +0200

    Added Instance Custom Resource Definition

--- cmd/main.go
+++ cmd/main.go
@@ -37,6 +37,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	kubebirdv1 "github.com/henryx/kubebird/api/v1"
+	"github.com/henryx/kubebird/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -48,6 +51,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(kubebirdv1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -219,6 +223,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.InstanceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "instance")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
